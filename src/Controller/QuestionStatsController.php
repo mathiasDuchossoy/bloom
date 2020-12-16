11168b99d34803dbbea2670452fd02b988cb6395
mathiasDuchossoy <mathias.duchossoy@gmail.com>
2020-12-16 03:00:27 +0100

    [upload csv file] 🚧 Browse the csv

--- src/Controller/QuestionStatsController.php
+++ src/Controller/QuestionStatsController.php
@@ -3,10 +3,13 @@
 
 namespace BloomAtWork\Controller;
 
+use League\Csv\Reader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
@@ -24,6 +27,33 @@ class QuestionStatsController extends AbstractController
      */
     public function readFile(Request $request)
     {
-        return new JsonResponse('Coucou');
+        try {
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $request->files->get('file');
+            if (!$uploadedFile) {
+                throw new BadRequestHttpException('"file" is required');
+            }
+
+            $fileName = $uploadedFile->getClientOriginalName();
+            $array = explode(".", $fileName);
+            $extension = end($array);
+
+            if ('csv' !== $extension) {
+                throw new BadRequestHttpException('csv file is required');
+            }
+
+            $csv = Reader::createFromPath($uploadedFile->getPathname(), 'r');
+            $csv->setHeaderOffset(0);
+
+            $header = $csv->getHeader();
+            $records = $csv->getRecords(['value']);
+            $label = str_replace('# ','', $header[0]);
+
+            foreach ($records as $record) {
+
+            }
+        } catch (\Exception $exception) {
+            return $this->json(['error' => $exception->getMessage()]);
+        }
     }
 }
